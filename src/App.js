aaacd1afcbc65c25418576d2ad22b3425b3a4044
JacksonFNX <85742256+JacksonFNX@users.noreply.github.com>
2022-11-19 21:02:02 -0300

    Projects-Update2

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ function App() {
       <div id= "Projects">
               <h2 className="titulos">Projetos</h2>
               <div className="container3">
-              <a className="link-projetos" href="https://jacksonfnx.github.io/acacafe/" target="_blank">
+              <a href="https://jacksonfnx.github.io/acacafe/" target="_blank">
               <div className="Project-block1">
                   Cafeteria (academico)
                 </div>
